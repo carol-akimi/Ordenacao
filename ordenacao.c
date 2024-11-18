879877a9e6bdc1cea5fc5ea8cb8e0f5a6eb06aff
carol-akimi <carolakimiueda@gmail.com>
2024-11-18 17:33:51 -0300

    Fiz a documentação

--- ordenacao.c
+++ ordenacao.c
@@ -56,6 +56,7 @@ void insertion_sort(int *vet, int n){
 }
 
 void shell_sort(int *vet, int inc[], int n, int n_inc){
+    //inc é o vetor que armazena os intervalos que serão feitos as ordenações
     int i, j; 
     int h; 
     //itera pelos elementos de inc, que armazena as distâncias dos sub-conjuntos
@@ -111,17 +112,24 @@ void quick_sort(int *vet, int inicio, int fim){
         quick_sort(vet, i, fim);
 }
 
+//Função que rearranja a heap em uma heap máxima, raiz com o maior elemento
 void rearranjar_heap(int *heap, int tam_heap, int i){
     int esq, dir, maior; 
+    //índices para acessar os filhos da esquerda e direita 
     esq = 2*i + 1;
     dir = 2*i + 2;
+    //armazena o índice do maior elemento 
     maior = i; 
+    //verifica se estamos acessando um índice válido, se sim, verifica 
+    //se existe um elemento maior que o que já está sendo armazenado 
     if ((esq < tam_heap) && (heap[esq] > heap[maior])){
         maior = esq; 
     }
     if ((dir < tam_heap) && (heap[dir] > heap[maior])){
         maior = dir; 
     }
+    //realiza a troca dos elementos de posição maior e i 
+    //chama recursivamente a função até que a heap esteja rearranjada
     if(maior != i){
         int aux = heap[maior];
         heap[maior] = heap[i];
@@ -130,15 +138,21 @@ void rearranjar_heap(int *heap, int tam_heap, int i){
     }
 }
 
+//Função que constrói uma heap máxima 
 void construir_heap(int *heap, int tam_heap){
+    //rearranja todos os nós menos os nós folhas 
+    //a cada chamada esses nós são raízes de heaps máximas
     for (int i = (tam_heap/2) - 1; i >= 0; i--){
         rearranjar_heap(heap, tam_heap, i); 
     }
 }
 
 void heap_sort(int *vet, int n){
+    //constrói a heap máxima
     construir_heap(vet, n); 
     int tam_heap = n, aux; 
+    //inverte de posição o nó raiz com o último nó da heap e rearranja 
+    //a heap aṕos a troca, isso é feito n-1 vezes até que esteja ordenado
     for (int i = n-1; i > 0; i--){
         aux = vet[0]; 
         vet[0] = vet[i]; 
@@ -149,6 +163,9 @@ void heap_sort(int *vet, int n){
 }
 
 void merge_sort(int *vet, int inicio, int fim){
+    //divide o vetor ao meio até que não seja mais possível e chama a função 
+    //recursiva para cada metade do vetor, após as divisões a função intercala 
+    //faz a junçã de forma ordenada 
     if (inicio < fim){
         int meio = (inicio+fim)/2; 
         merge_sort(vet, inicio, meio);
@@ -157,27 +174,32 @@ void merge_sort(int *vet, int inicio, int fim){
     }
 }
 
+//Função que faz a junção de dois subvetores, de forma que eles fiquem ordenados 
 void intercala(int *vet, int inicio, int meio, int fim){
     int i, j, k;
     int n1, n2;
-    //Tamanho dos vetores 
+    //tamanho dos sub-vetoes 
     n1 = meio - inicio + 1;
     n2 = fim - meio; 
 
+    //declaração dos sub/vetores 
     int L[n1+1], R[n2+1]; 
 
-    //Preenchimento dos vetores 
+    //preenchimento dos vetores 
     for (i = 0; i < n1; i++){
         L[i] = vet[inicio + i]; 
     }
-    L[n1] = INT_MAX; //sentinela
+    L[n1] = INT_MAX; //Sentinela
     for (j = 0; j < n2; j++){
         R[j] = vet[meio + j + 1]; 
     } 
-    R[n2] = INT_MAX;  //sentinela2
+    R[n2] = INT_MAX; //Sentinela
 
     i = 0; j = 0; 
     for (k = inicio; k <= fim; k++){
+        //compara os elementos de cada metade e os organiza de forma ordenada no 
+        //vetor vet. O sentinela garante que se um dos sub-vetores não tiver elementos,
+        //o restante vai ser preenchido pelo outro sub-vetor.  
         if (L[i] <= R[j]){
             vet[k] = L[i]; 
             i++; 
@@ -189,23 +211,27 @@ void intercala(int *vet, int inicio, int meio, int fim){
 }
 
 int *contagem_de_menores(int *vet, int n){
+    //armazena quantos elementos são menores que o elemento daquela posição
     int *posicao = (int*)malloc(sizeof(int)*n); 
-    //zerar o vetor posicao 
+    //zerar o vetor posição 
     for (int i = 0; i < n; i++){
         posicao[i] = 0; 
     }
+    //conta quantos elementos são menores que cada item
     for (int i = 1; i < n; i++){
         for (int j = i - 1; j >= 0; j--){
             if (vet[i] < vet[j]){
                 posicao[j]++; 
-
             }
             else{
                 posicao[i]++;
             }   
         }
     }
+    //vetor em que serão colocados os elementos ordenados
     int *sorted = (int*)malloc(sizeof(int)*n); 
+    //o vetor sorted recebe o elemento no índice posicão[i], ou seja, 
+    //considerando quantos elementos são menores que o analisado
     for (int i = 0; i < n; i++){
         sorted[posicao[i]] = vet[i]; 
     }
@@ -213,7 +239,9 @@ int *contagem_de_menores(int *vet, int n){
 }
 
 int *radixsort(int *vet, int tam, int n_dig){
-    //int n_dig = numero_digitos(vet, tam); 
+    //n_dig armazena a quantidade máxima de dígitos que cada número possui
+    //loop que itera por todos os dígitos do número, os ordenado os ordenando
+    //do mais ao menos significativo utilizando o algoritmo counting sort 
     for(int i = 1; i <= n_dig; i++){
         vet = counting_sort(vet, tam, i);
     }
@@ -221,15 +249,24 @@ int *radixsort(int *vet, int tam, int n_dig){
 }
 
 int *counting_sort(int *vet, int tam, int pos){
+    //cada índice corresponde a um número, e a quantidade que essa posição
+    //armazena é a quantidade de vezes que esse número aparece no vetor
+    //nesse caso, os números são apenas de 0-9
     int tipos[10] = {0};
+    //conta quantos dígitos de cada número aparecem 
     for(int i = 0; i < tam; i++){
         tipos[digito(vet[i], pos)]++;
     }
+    //soma com a quantidade de vezes que o núemro anterior aparece para fornecer 
+    //a posição certa em que esse número deve ocupar no vetor ordenado
     for(int i = 1; i < 10; i++)
         tipos[i] += tipos[i-1];
 
+    //vetor ordenado 
     int* sorted = malloc(tam * sizeof(int));
-
+    //armazena os elementos no vetor ordenado de acordo com sua posição
+    //determinada pelo vetor tipos, faz um decremento no vetor tipos,
+    //pois a cada iteração temos menos um elemento daquele para ser inserido
     for(int i = tam-1; i >= 0; i--){
         sorted[tipos[digito(vet[i], pos)]-1] = vet[i];
         tipos[digito(vet[i], pos)]--;
@@ -238,6 +275,7 @@ int *counting_sort(int *vet, int tam, int pos){
     return sorted;
 }
 
+//Função que devolve qual digito está na posição pos 
 int digito(int num, int pos){
     for(int i=0; i<(pos-1); i++)
         num /= 10;
